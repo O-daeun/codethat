f723760093b75babc508a32ed923261cc4eb2ab1
daeun <qzsz797979@gmail.com>
2024-03-27 10:30:03 +0900

    카탈로그, 커뮤니티 하위 페이지 동적으로 경로 작업 완료

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-import ReactDOM from 'react-dom';
-import Main from './Main';
+import ReactDOM from "react-dom/client";
+import Main from "./Main";
+import React from "react";
 
-ReactDOM.render(<Main />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>
+);
